198911129116148c23ba99b7430524bf753cf2d0
EngineMachiner <mfpc98@gmail.com>
2024-05-17 21:47:30 -0500

    Update Math.kt
    + Add Vec3f to Vec3i conversion.

--- src/main/kotlin/com/enginemachiner/harmony/Math.kt
+++ src/main/kotlin/com/enginemachiner/harmony/Math.kt
@@ -2,6 +2,7 @@ package com.enginemachiner.harmony
 
 import net.minecraft.util.math.Vec3d
 import net.minecraft.util.math.Vec3f
+import net.minecraft.util.math.Vec3i
 import kotlin.math.PI
 
 /** Convert degree to radians. */
@@ -11,5 +12,11 @@ fun rad(angle: Double): Double { return angle % 360 * 2 * PI / 360 }
 fun vec3d(vec3f: Vec3f): Vec3d {
 
     return Vec3d( vec3f.x.toDouble(), vec3f.y.toDouble(), vec3f.z.toDouble() )
+
+}
+
+fun vec3i(vec3f: Vec3f): Vec3i {
+
+    return Vec3i( vec3f.x.toInt(), vec3f.y.toInt(), vec3f.z.toInt() )
 
 }
