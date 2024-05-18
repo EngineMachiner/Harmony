446cb2f18eb3b494757f0747cc8adc98eab5e9d6
EngineMachiner <mfpc98@gmail.com>
2024-05-17 20:17:38 -0500

    Update Math.kt
    + Add Vec3f to Vec3d conversion.

--- src/main/kotlin/com/enginemachiner/harmony/Math.kt
+++ src/main/kotlin/com/enginemachiner/harmony/Math.kt
@@ -1,6 +1,15 @@
 package com.enginemachiner.harmony
 
+import net.minecraft.util.math.Vec3d
+import net.minecraft.util.math.Vec3f
 import kotlin.math.PI
 
 /** Convert degree to radians. */
 fun rad(angle: Double): Double { return angle % 360 * 2 * PI / 360 }
+
+/** Convert a Vec3f into a Vec3d. */
+fun vec3d(vec3f: Vec3f): Vec3d {
+
+    return Vec3d( vec3f.x.toDouble(), vec3f.y.toDouble(), vec3f.z.toDouble() )
+
+}
