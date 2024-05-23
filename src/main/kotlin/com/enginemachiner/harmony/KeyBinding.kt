4b6edbda31b5b77b19f22bb2125425c05e2ed0a8
EngineMachiner <mfpc98@gmail.com>
2024-05-22 22:09:57 -0500

    Update KeyBinding.kt

--- src/main/kotlin/com/enginemachiner/harmony/KeyBinding.kt
+++ src/main/kotlin/com/enginemachiner/harmony/KeyBinding.kt
@@ -3,12 +3,13 @@ package com.enginemachiner.harmony
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
 import net.minecraft.client.option.KeyBinding
 import net.minecraft.client.util.InputUtil
+import org.lwjgl.glfw.GLFW
 
 object KeyBinding {
 
     private val CATEGORY = "category.$MOD_NAME"
 
-    private fun register( key: String, type: InputUtil.Type, glfwCode: Int ): KeyBinding {
+    private fun register( key: String, type: InputUtil.Type = InputUtil.Type.KEYSYM, glfwCode: Int = GLFW.GLFW_KEY_UNKNOWN ): KeyBinding {
 
         val preKey = "key.$MOD_NAME."
 
