5cff7d315ea28e301d2cb153c8c18f200b763126
EngineMachiner <mfpc98@gmail.com>
2024-05-29 23:06:42 -0500

    Update Message.kt

--- src/client/kotlin/com/enginemachiner/harmony/client/Message.kt
+++ src/client/kotlin/com/enginemachiner/harmony/client/Message.kt
@@ -5,8 +5,12 @@ import com.enginemachiner.harmony.Message
 //** Sends messages and warnings to the client. */
 class Message( message: String,     private val exception: Exception? = null ) : Message( message, player() ) {
 
-    private fun exception() { exception?.printStackTrace() };           fun console() { send() }
+    private fun exception() { exception?.printStackTrace() }
 
     override fun send( actionBar: Boolean ) { super.send(actionBar);      exception() }
 
+    fun console() { sendMessage("message.check_console"); send() }
+
 }
+
+fun sendMessage( message: String, exception: Exception? = null ) { Message(message, exception).send() }
