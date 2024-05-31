0ef2381092a29da24186a165475405376a4cae1d
EngineMachiner <mfpc98@gmail.com>
2024-05-30 21:06:59 -0500

    Update Translation.kt
    + Fix Translation.has()

--- src/main/kotlin/com/enginemachiner/harmony/Translation.kt
+++ src/main/kotlin/com/enginemachiner/harmony/Translation.kt
@@ -5,9 +5,7 @@ import net.minecraft.util.Language
 
 object Translation {
 
-    private val language: Language = Language.getInstance()
-
-    fun has( key: String ): Boolean { return language.hasTranslation("$MOD_NAME.$key") }
+    fun has( key: String ): Boolean { return get(key) != "$MOD_NAME.$key" }
     fun get( key: String ): String { return Text.translatable( "$MOD_NAME.$key" ).string }
     fun item( key: String ): String { return Text.translatable( "item.$MOD_NAME.$key" ).string }
     fun block( key: String ): String { return Text.translatable( "block.$MOD_NAME.$key" ).string }
