d3a1eb553e9bfd7840e6d5f44445db7f8462e784
EngineMachiner <mfpc98@gmail.com>
2024-06-05 10:03:51 -0500

    Create Renderer.kt

---
+++ src/client/kotlin/com/enginemachiner/harmony/client/Renderer.kt
@@ -0,0 +1,27 @@
+package com.enginemachiner.harmony.client
+
+import net.fabricmc.fabric.api.client.rendering.v1.BuiltinItemRendererRegistry
+import net.minecraft.client.render.VertexConsumerProvider
+import net.minecraft.client.render.model.json.ModelTransformation
+import net.minecraft.client.util.math.MatrixStack
+import net.minecraft.item.ItemStack
+
+private typealias itemRegistry = BuiltinItemRendererRegistry
+private typealias itemDynamic = BuiltinItemRendererRegistry.DynamicItemRenderer
+private typealias itemRender = (ItemStack, ModelTransformation.Mode, MatrixStack, VertexConsumerProvider, Int, Int ) -> Unit
+
+object Renderer {
+
+    object Item {
+
+        val registry: itemRegistry = BuiltinItemRendererRegistry.INSTANCE
+
+        fun create( render: itemRender ): itemDynamic {
+
+            return BuiltinItemRendererRegistry.DynamicItemRenderer(render)
+
+        }
+
+    }
+
+}
