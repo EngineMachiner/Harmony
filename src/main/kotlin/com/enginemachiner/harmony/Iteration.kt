2c7c586358858ff9da3eecf229efb3ea82a985ce
EngineMachiner <mfpc98@gmail.com>
2024-07-03 22:36:23 -0500

    Update Iteration.kt
    Forgot the i

--- src/main/kotlin/com/enginemachiner/harmony/Iteration.kt
+++ src/main/kotlin/com/enginemachiner/harmony/Iteration.kt
@@ -3,7 +3,7 @@ package com.enginemachiner.harmony
 /** Gets value and cycles if the index is out of bounds. */
 fun <T: Any> cycle( collection: Collection<T>, value: T, i: Int = 1 ): T {
 
-    val size = collection.size;         val i = collection.indexOf(value) + 1
+    val size = collection.size;         val i = collection.indexOf(value) + i
 
     return collection.elementAt( i % size )
 
