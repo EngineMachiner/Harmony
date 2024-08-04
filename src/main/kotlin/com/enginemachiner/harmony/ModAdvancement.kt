2b15360b1eddd4168cbe04fd82c60920f83f1798
EngineMachiner <mfpc98@gmail.com>
2024-08-03 22:39:01 -0500

    Update ModAdvancement.kt

--- src/main/kotlin/com/enginemachiner/harmony/ModAdvancement.kt
+++ src/main/kotlin/com/enginemachiner/harmony/ModAdvancement.kt
@@ -12,35 +12,32 @@ private typealias Conditions = AbstractCriterionConditions
 /** Advancement builder for custom mod advancements in Data Generation. */
 abstract class ModAdvancement : ModID {
 
-    open val name: String = lazy { className() }.value
-
     abstract val icon: Item
 
-    open val parent: ModAdvancement? = null
+    open val name: String = lazy { className() }.value
 
+    open val parent: ModAdvancement? = null
 
     open val key: String = lazy { name }.value
 
     open fun title(): Text { return Translation.Advancement.title(key) }
     open fun description(): Text { return Translation.Advancement.description(key) }
 
-
     open val frame = AdvancementFrame.TASK
     open val background = Identifier("textures/gui/advancements/backgrounds/adventure.png")
-
 
     private val toast = false;          private val announce = false
     private val hidden = false
 
-
     private var advancement: Advancement? = null
 
+    abstract fun conditions(): AbstractCriterionConditions
 
-    fun build( consumer: Consumer<Advancement>, conditions: Conditions, criterion: String = name ): Advancement {
+    fun build( consumer: Consumer<Advancement>, criterion: String = name ): Advancement {
 
         val advancement = Advancement.Builder.create()
             .display( icon, title(), description(), background, frame, toast, announce, hidden )
-            .parent( parent?.advancement ).criterion( criterion, conditions )
+            .parent( parent?.advancement ).criterion( criterion, conditions() )
             .build( consumer, "$MOD_NAME/$name" )
 
         this.advancement = advancement;         return advancement
