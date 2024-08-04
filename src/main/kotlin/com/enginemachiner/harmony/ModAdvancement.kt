0f0cb8e5b4c46317efb0ace9cb98c6a5b099d143
EngineMachiner <mfpc98@gmail.com>
2024-08-03 21:21:38 -0500

    Update ModAdvancement.kt
    + Make it abstract and override everything instead.

--- src/main/kotlin/com/enginemachiner/harmony/ModAdvancement.kt
+++ src/main/kotlin/com/enginemachiner/harmony/ModAdvancement.kt
@@ -4,47 +4,40 @@ import net.minecraft.advancement.Advancement
 import net.minecraft.advancement.AdvancementFrame
 import net.minecraft.advancement.criterion.AbstractCriterionConditions
 import net.minecraft.item.ItemStack
+import net.minecraft.text.Text
 import net.minecraft.util.Identifier
 import java.util.function.Consumer
 
 private typealias Conditions = AbstractCriterionConditions
 
 /** Advancement builder for custom mod advancements in Data Generation. */
-open class ModAdvancement(
+abstract class ModAdvancement(
 
-    val name: String,
-
-    titleKey: String?,           descriptionKey: String?,
-
-    val icon: ItemStack,        val parent: Advancement? = null
+    val name: String,       open val icon: ItemStack,        val parent: ModAdvancement? = null
 
 ) {
 
-    /** Constructor used if there's a custom implementation with title and description override. */
-    constructor( name: String, icon: ItemStack, parent: Advancement ) : this( name, null, null, icon, parent )
+    abstract val titleKey: String;              abstract val descriptionKey: String
 
-    protected open val title = Translation.Advancement.title( titleKey!! )
-    protected open val description = Translation.Advancement.description( descriptionKey!! )
+    open fun title(): Text { return Translation.Advancement.title( titleKey ) }
+    open fun description(): Text { return Translation.Advancement.description( descriptionKey ) }
 
-    var frame = AdvancementFrame.TASK
+    open val frame = AdvancementFrame.TASK
+    open val background = Identifier("textures/gui/advancements/backgrounds/adventure.png")
 
-    var background = Identifier("textures/gui/advancements/backgrounds/adventure.png")
+    private val toast = false;          private val announce = false
+    private val hidden = false
 
-    private var toast = false;          private var announce = false
-    private var hidden = false
+    private var advancement: Advancement? = null
 
-    fun toast(): ModAdvancement { toast = true;     return this }
-    fun announce(): ModAdvancement { announce = true;     return this }
-    fun hidden(): ModAdvancement { hidden = true;     return this }
+    fun build( consumer: Consumer<Advancement>, conditions: Conditions, criterion: String = name ): Advancement {
 
-    fun build( consumer: Consumer<Advancement>, conditions: Conditions, criterion: String = name ) {
+        val advancement = Advancement.Builder.create()
+            .display( icon, title(), description(), background, frame, toast, announce, hidden )
+            .parent( parent?.advancement ).criterion( criterion, conditions )
+            .build( consumer, "$MOD_NAME/$name" )
 
-        val builder = Advancement.Builder.create()
-
-        if ( parent != null ) builder.parent(parent)
-
-        builder.display( icon, title, description, background, frame, toast, announce, hidden )
-            .criterion( criterion, conditions ).build( consumer, "$MOD_NAME/$name" )
+        this.advancement = advancement;         return advancement
 
     }
 
