78013b0fbe915c3db50046d984f5e3320a63a1b8
EngineMachiner <mfpc98@gmail.com>
2025-10-11 15:40:58 -0500

    oops translations are needed for advancements

--- src/main/kotlin/com/enginemachiner/harmony/Mod.kt
+++ src/main/kotlin/com/enginemachiner/harmony/Mod.kt
@@ -18,6 +18,9 @@ open class Mod( val name: String ) {
     /**@see Debug */
     open val debug = Debug(this)
 
+    /**@see Translation */
+    open val translation = Translation(name)
+
     fun nbt( nbt: NbtCompound ) = nbt.getCompound(name)!!
 
     fun nbt( itemStack: ItemStack ) = nbt( itemStack.nbt!! )
